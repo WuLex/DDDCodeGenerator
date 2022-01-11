df62e50d46ce3ce96ad03382e4db691fae907e9d
wulex <827937686@qq.com>
2022-01-11 23:25:37 +0800

    初始化项目

---
+++ CodeGenerator/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using CodeGenerator.Common;
+using CodeGenerator.Helper;
+using CodeGenerator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IO.Compression;
+
+namespace CodeGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile file)
+        {
+            var tables = new List<Table>();
+            if (file == null)
+            {
+                return View();
+            }
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    var name = reader.ReadLine();
+                    if (name != null)
+                    {
+                        tables.Add(new Table()
+                        {
+                            Name = name.ToString().Replace("\"", ""),
+                            GenerateFile = true
+                        });
+                    }
+                }
+            }
+            TempData["Tables"] = JsonConvert.SerializeObject(tables.OrderBy(x => x.Name).ToList());
+            return RedirectToAction("NamespaceAndTableSelection");
+        }
+
+        public IActionResult NamespaceAndTableSelection()
+        {
+            var tablesSerialized = TempData["Tables"];
+            if (tablesSerialized != null)
+            {
+                var tables = JsonConvert.DeserializeObject<List<Table>>(tablesSerialized.ToString());
+                var tableViewModel = new TableViewModel() { Tables = tables };
+                return View(tableViewModel);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult NamespaceAndTableSelection(TableViewModel tableViewModel)
+        {
+            //项目路径
+            string filesPath = Directory.GetCurrentDirectory() + "\\Example\\";
+            //string path = MyServiceProvider.ServiceProvider.GetRequiredService<IHostEnvironment>().ContentRootPath;
+
+            var pathZip = Directory.GetCurrentDirectory() + @"\CreatedZipFile";
+            var pathToDownload = Directory.GetCurrentDirectory() + @"\DownloadFiles";
+
+            if (tableViewModel == null || tableViewModel.Tables == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var filesNameToGenerate = Directory.GetFiles(filesPath).ToList();
+            FileHelper.DeleteOlderFiles(filesNameToGenerate, pathToDownload);
+
+            foreach (var table in tableViewModel.Tables)
+            {
+                foreach (var templateFilePath in filesNameToGenerate)
+                {
+                    if (table.GenerateFile)
+                    {
+                        FileHelper.CreateFiles(templateFilePath, tableViewModel.Namespace, table.Name);
+                    }
+                }
+            }
+            var directoryInfo = new DirectoryInfo(pathZip);
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                file.Delete();
+            }
+
+            var zipName = "Code" + tableViewModel.Namespace + ".zip";
+            ZipFile.CreateFromDirectory(pathToDownload, pathZip + @"\" + zipName, CompressionLevel.Fastest, false);
+            FileHelper.DeleteOlderFiles(filesNameToGenerate, pathToDownload);
+
+            byte[] bytes = System.IO.File.ReadAllBytes(pathZip + @"\" + zipName);
+
+            return File(bytes, "application/octet-stream", zipName);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CodeGenerator/Helper/Helper.cs
@@ -0,0 +1,90 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CodeGenerator.Helper
+{
+    public static class FileHelper
+    {
+        /// <summary>
+        /// 驼峰式
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string ToCamelCase(string text)
+        {
+            TextInfo txtInfo = new CultureInfo("pt-br", false).TextInfo;
+            return txtInfo.ToTitleCase(text).Replace("_", string.Empty);
+        }
+
+        /// <summary>
+        /// 驼峰式首字母小写
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string ToCamelCaseLoweFirst(string text)
+        {
+            TextInfo txtInfo = new CultureInfo("zh-CN", false).TextInfo;
+            text = txtInfo.ToTitleCase(text).Replace("_", string.Empty);
+            return Char.ToLowerInvariant(text[0]) + text.Substring(1);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="templateFilePath"></param>
+        /// <param name="nameSpace"></param>
+        /// <param name="className"></param>
+        public static void CreateFiles(string templateFilePath, string nameSpace, string className)
+        {
+            StreamReader rd = new StreamReader(templateFilePath);
+            string strContents = rd.ReadToEnd();
+            rd.Close();
+
+            Regex regexText = new Regex("#NAMESPACE#");
+            strContents = regexText.Replace(strContents, ToCamelCase(nameSpace));
+            regexText = new Regex("#CLASSNAME#");
+            strContents = regexText.Replace(strContents, ToCamelCase(className));
+            regexText = new Regex("#VARCLASSNAME#");
+            strContents = regexText.Replace(strContents, ToCamelCaseLoweFirst(className));
+
+            string pathToSave = Directory.GetCurrentDirectory() + @"\DownloadFiles\" +
+                                Path.GetFileNameWithoutExtension(templateFilePath).Replace(".", @"\");
+
+            string fileName = className + "." + "cs";
+
+            var path = Path.Combine(pathToSave, fileName);
+
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.Write(strContents);
+                    sw.Close();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filesNameToGenerate"></param>
+        /// <param name="downloadFilePath"></param>
+        public static void DeleteOlderFiles(List<string> filesNameToGenerate, string downloadFilePath)
+        {
+            foreach (var templateFilePath in filesNameToGenerate)
+            {
+                var pathFileToDelete = downloadFilePath + @"\" + Path.GetFileNameWithoutExtension(templateFilePath).Replace(".", @"\");
+                var directoryInfo = new DirectoryInfo(pathFileToDelete);
+
+                if (directoryInfo.GetFiles().Count() > 0)
+                {
+                    foreach (FileInfo file in directoryInfo.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                }
+            }
+        }
+    }
+}
